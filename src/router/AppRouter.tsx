c79108124190d2adc8d6892ecb9bb0d94c75b4fe
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-20 15:28:20 -0500

    Add LoginWithPhone route to AppRouter

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -4,13 +4,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import PublicRoutes from "./PublicRouter";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import LoginWithPhone from "../pages/LoginWithPhone";
 
 function AppRouter(){
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="Login" element={<Login/>} />
-                <Route path="loginWithPhone" element={<div>Home</div>} />
+                <Route path="loginWithPhone" element={<LoginWithPhone/>} />
                 <Route path="Register" element={<Register/>} />
             </Routes>
         </BrowserRouter>
