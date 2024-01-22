f055034293fd20c29a46a8bc6b9cdd603da239a5
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-22 17:28:38 -0500

    Refactor search input and add member button
    This commit refactors the search input in the UsersTable component and adds a new "Add member" button.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,18 @@
 // import Sidebar from "../../components/Sidebar"
-// import Header from "../../components/Header"
-// import UsersTable from "../../components/UsersTable"
+import Header from "../../components/Header"
+import UsersTable from "../../components/UsersTable"
 import Link from "../../components/Link"
 
 
 const Home = () => {
     return (
-        < div >
+        < div className="flex flex-col
+        p-14
+        gap-10
+        ">
+            <Header/>
             <Link/>
+            <UsersTable/>
         </div>
     )
 }
