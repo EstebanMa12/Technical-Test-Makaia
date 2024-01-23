3207e9e82e885a84aa546ed73421cf05479be684
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-23 11:43:41 -0500

    Refactor DropdownCheckbox component to handle "All Projects" option

--- src/components/DropdownCheckbox/index.tsx
+++ src/components/DropdownCheckbox/index.tsx
@@ -3,15 +3,31 @@ import { useState } from 'react';
 const DropdownCheckbox = ({ options, onChange }: { options: string[], onChange: (options: string[]) => void }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+  const [toggleName, setToggleName] = useState<string>('Select Projects');
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
 
   const handleOptionChange = (option: string) => {
-    const updatedOptions = selectedOptions.includes(option)
+    let updatedOptions: string[] = [];
+    if (option === 'All Projects') {
+      updatedOptions = options;
+      setToggleName('All Projects');
+      onChange(['All Projects']);
+      return;
+    }else{
+      updatedOptions = selectedOptions.includes(option)
       ? selectedOptions.filter((selectedOption: string) => selectedOption !== option)
       : [...selectedOptions, option];
+      if(updatedOptions.length === 0){
+        setToggleName('Select Projects');
+      }
+      else{
+        setToggleName(updatedOptions.join(', '));
+      }
+    }
+
 
     setSelectedOptions(updatedOptions);
     onChange(updatedOptions);
@@ -23,7 +39,7 @@ const DropdownCheckbox = ({ options, onChange }: { options: string[], onChange: 
         className="dropdown-header block p-2 bg-gray-50 rounded-lg w-60 text-sm text-gray-400 border border-gray-300 focus:ring-pink-500 focus:border-pink-500 focus:text-pink-700 focus:font-bold active:border-pink-500 active:ring-pink-500 active:ring-2"
         onClick={toggleDropdown}
       >
-        Select Projects
+        {toggleName}
       </div>
       {isOpen && (
         <div className="absolute top-full left-0 z-10 block p-2 shadow-md bg-white w-60">
