87a8459d563b01f8692231f61d5401b7c15b7b75
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-23 17:28:52 -0500

    Add Redux store provider to AppRouter component

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppRouter from './router/AppRouter.tsx'
 import './styles/index.css'
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppRouter/>
+    <Provider store={store}>
+      <AppRouter/>  
+    </Provider>
   </React.StrictMode>,
 )
