4291685ba9ecb256d17517ed846e222fe2e78cc3
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-23 17:44:29 -0500

    Update imports in main.tsx and remove unused files

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import AppRouter from './router/AppRouter.tsx'
 import './styles/index.css'
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store } from "./redux/store.ts";
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
